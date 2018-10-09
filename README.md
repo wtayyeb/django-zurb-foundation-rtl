6c32400b422ee397f4d31817fb6c2f8d9262eedf
Tayyeb <tayyeb@tsaze.com>
2018-10-09 18:13:48 +0330

    bump version to 6.5.0-rc.3

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-static-foundation 6.3.1
+static-foundation 6.5.0-rc.3
 ============================
 
-Django Zurb Foundation 6.3.1 package.
+Django Zurb Foundation 6.5.0-rc.3 package.
 
 Version of this package is equal to the version of Zurb Foundation it provides,
 plus the patch counter of our modifications
--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 # /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
 import os
+
+from setuptools import setup, find_packages
 
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
 
 setup(
     name="static-foundation",
-    version="6.3.1.0",
+    version="6.5.0-rc.3",
     description="Django Zurb Foundation package",
+    long_description=open('Readme.md').read(),
     author="w.tayyeb",
     author_email="w.tayyeb@gmail.com",
     url="https://github.com/wtayyeb/static-foundation",

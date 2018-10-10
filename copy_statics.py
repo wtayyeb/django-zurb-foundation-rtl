1276a03f06ec1863cf4dca3983425c4673cffe85
Tayyeb <tayyeb@tsaze.com>
2018-10-10 13:02:53 +0330

    udate copy_statics.py to collect motion-ui

--- copy_statics.py
+++ copy_statics.py
@@ -14,6 +14,8 @@ import shutil
 def main():
     zf = r'zurb-foundation-sites/'
     df = r'foundation/static/foundation/'
+    zm = r'zurb-motion-ui/'
+    dm = r'foundation/static/motion-ui/'
 
     pathes = (
         (glob.glob(zf + 'scss'), df + 'scss'),
@@ -23,18 +25,28 @@ def main():
         (glob.glob(zf + 'dist/css'), df + 'css'),
         (glob.glob(zf + 'dist/js'), df + 'js'),
         (zf + 'LICENSE', df),
+
+        (glob.glob(zm + 'src'), dm + 'scss'),
+        (glob.glob(zm + 'dist/*.css'), dm + 'css', True),
+        (glob.glob(zm + 'dist/*.js'), dm + 'js', True),
+        (zm + 'LICENSE', dm),
     )
-    for src, dst in pathes:
+    for row in pathes:
+        src, dst, mkdir = (row + (False,))[0:3]
         print('copy %s to %s' % (src, dst))
 
         if isinstance(src, list):
             try:
                 shutil.rmtree(dst)
             except OSError as e:
-                if e.errno == 2:
-                    pass
-                else:
+                if e.errno not in (errno.ENOENT, errno.EINVAL):
                     raise
+
+            if mkdir:
+                try:
+                    os.makedirs(dst)
+                except OSError:
+                    pass
 
             for s in src:
                 cp(s, dst)
@@ -51,7 +63,7 @@ def cp(src, dst):
         shutil.copytree(src, dst)
 
     except OSError as exc:
-        if exc.errno == errno.ENOTDIR:
+        if exc.errno in (errno.ENOTDIR, errno.EINVAL):
             shutil.copy(src, dst)
         else:
             raise
